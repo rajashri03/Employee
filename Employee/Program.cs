c1a355592f4312b8fb2e297576e1c6d2a018b906
rajashri03 <telvekar.rajashri@gmail.com>
2022-11-30 08:16:36 +0530

    [Rajashri]-Check emp is present or absent

---
+++ Employee/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employee Absent/Present");
+        }
+    }
+}
+//UC1-Emplyee absent/presnt
+//UC2-calculate salary
